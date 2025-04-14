ec08a8afa700f33a39fe5e2fd234b3afa935fda2
mubaris <mubarikunnath@gmail.com>
2025-04-14 21:31:45 +0530

    Refactor indentation in models.py for Tag and Snippet classes

--- snip/models.py
+++ snip/models.py
@@ -1,28 +1,28 @@
-    from django.db import models
-    from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
 
-    class Tag(models.Model):
-        user = models.ForeignKey(
-            User, on_delete=models.CASCADE, related_name="tags"
-        )
-        title = models.CharField(max_length=100, unique=True)
+class Tag(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tags"
+    )
+    title = models.CharField(max_length=100, unique=True)
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
 
 
-    class Snippet(models.Model):
-        title = models.CharField(max_length=100)
-        note = models.TextField()
-        created_at = models.DateTimeField(auto_now_add=True)
-        updated_at = models.DateTimeField(auto_now=True)
-        user = models.ForeignKey(
-            User, on_delete=models.CASCADE, related_name="snippets"
-        )
-        tag = models.ForeignKey(
-            Tag, on_delete=models.SET_NULL, null=True, blank=True
-        )
+class Snippet(models.Model):
+    title = models.CharField(max_length=100)
+    note = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="snippets"
+    )
+    tag = models.ForeignKey(
+        Tag, on_delete=models.SET_NULL, null=True, blank=True
+    )
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
